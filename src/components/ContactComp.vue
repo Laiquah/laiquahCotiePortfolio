32b4d4321a764c66c5e0e374edffaa38f0551c65
laiquah <cotielaiquah@gmail.com>
2023-07-02 09:20:19 +0200

    added contact page its not done

---
+++ src/components/ContactComp.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <form action="https://formspree.io/f/mgejwqav" method="POST">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">First name:</label>
+                <center><input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name" required></center>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Last surname:</label>
+                <center><input type="email" class="form-control" id="exampleFormControlInput1" placeholder="surname" required></center>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address:</label>
+                <center><input type="email" class="form-control" id="exampleFormControlInput1" placeholder="person@gmail.com" required></center>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Contact Number:</label>
+                <center><input type="email" class="form-control" id="exampleFormControlInput1" placeholder="065 819 0250" required></center>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea:</label>
+                <center><textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Leave your message here"></textarea></center>
+            </div>
+            <button>Submit</button>
+            <button>Clear</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+button{
+    margin: 1rem;
+    background-color: #FF95E1;
+    padding: .5rem;
+    width: 10rem;
+    border-radius: 5rem;
+}
+
+button:hover{
+    background-color: #FFE0F6;
+}
+
+form{
+    margin-bottom: 1rem;
+}
+
+input{
+    width: 20rem;
+}
+
+textarea{
+    width: 20rem;
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         <h1>Contact me:</h1>
+        <Contact/>
     </div>
 </template>
 
 <script>
+import Contact from '../components/ContactComp.vue'
     export default {
-        
+        components: {
+            Contact
+        }
     }
 </script>
 
@@ -14,5 +18,9 @@
 h1{
     text-decoration: underline;
     font-weight: 700;
+}
+
+div{
+    background-color: #FFE0F6;
 }
 </style>
