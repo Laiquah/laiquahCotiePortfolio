b4a239a817a4ea1228b61d9b78377063f7154f14
laiquah <cotielaiquah@gmail.com>
2023-07-05 21:34:02 +0200

    added info

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+// import '@fortawesome/fontawesome-free/css/all.css'
+// import '@fortawesome/fontawesome-free/js/all.js'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(AOS.init()).mount('#app')
